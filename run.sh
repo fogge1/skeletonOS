acc1b70d093a8fa5d7e3a69b0eacdaaac7a3654d
Isak Fogelberg <isak.fogge1@gmail.com>
2022-12-20 10:48:27 +0100

    print in text mode without bios

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-
-
 qemu-system-i386 -drive format=raw,file=skeletonOS.iso
--- src/boot.asm
+++ src/boot.asm
@@ -1,28 +1,76 @@
+; vim: ft=nasm
 [bits 16]
   mov ax, 0x07c0
   mov ds, ax
+  mov ss, ax
+  mov sp, 0x9c00
+
   cld
+  
+  mov ax, 0xb800,
+  mov es, ax
 
   mov si, msg 
-  call print_bios
+  call sprint
+
+  mov ax, 0xb800
+  mov gs, ax
+  mov bx, 0x0000
+  mov ax, [gs:bx]
+  mov word [reg16], ax
+  call printreg16
 
 hang: 
   jmp hang
 
-msg: db 'Hello world', 13, 10, 0
 
-print_bios:
-  lodsb
-  or al, al
-  jz done
-  mov ah, 0x0E
-  mov bh, 0
-  int 0x10
-  jmp print_bios
-
-done:
+dochar: call cprint
+sprint: lodsb
+  cmp al, 0
+  jne dochar
+  add byte [ypos], 1
+  mov byte [xpos], 0
   ret
 
-  times 510-($-$$) db 0
-  db 0x55
-  db 0xAA
+cprint: mov ah, 0x0F
+  mov cx, ax
+  movzx ax, byte [ypos]
+  mov dx, 160
+  mul dx
+  movzx bx, byte [xpos]
+  shl bx, 1
+
+  mov di, 0
+  add di, ax
+  add di, bx
+
+  mov ax, cx
+  stosw
+  add byte [xpos], 1 
+
+  ret
+printreg16:
+  mov di, outstr16
+  mov ax, [reg16]
+  mov si, hexstr
+  mov cx, 4
+
+hexloop:
+  rol ax, 4
+  mov bx, ax
+  and bx, 0x0f
+  mov bl, [si + bx]
+  mov [di], bl
+  inc di
+  dec cx
+  jnz hexloop
+
+xpos db 12
+ypos db 12
+hexstr db '0123456789ABCDEF'
+outstr16 db '0000', 0
+reg16 dw 0
+msg: db 'Hello world', 0
+times 510-($-$$) db 0
+db 0x55
+db 0xAA
